344a4a7cdc699e7b8ba2461fb5116e65fc368101
Sebastianb <sebastianb.0211@gmail.com>
2023-08-31 16:34:26 -0500

    se sube pagina del proyecto sura

---
+++ assets/js/fs.js
@@ -0,0 +1,15 @@
+const sayHi=()=>{
+     if guias[0]{
+          s aksas
+
+
+
+
+
+     
+          s cugaks  jbaiosnkqñibakgcia  jbaibdaj  iab ajbxa sj ajs xjbajsjas jx aj sxjasj a sjx ajs  ajbsi asabsihaissebas as
+     }
+
+
+
+}
